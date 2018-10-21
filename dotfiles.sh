7ba847915ba7746a730cfd8f05682831c63ab22f
Ryan Price <ryapric@gmail.com>
2018-10-21 17:25:02 -0500

    Add RStudio block to .

--- dotfiles.sh
+++ dotfiles.sh
@@ -11,17 +11,27 @@ if [ "$1" == "-u" ]; then
     echo "Uploading dotfile settings..."
     
     # RStudio
+    echo "RStudio config..."
     mkdir -p .rstudio-desktop/monitored/user-settings/
     cp ~/.rstudio-desktop/monitored/user-settings/user-settings .rstudio-desktop/monitored/user-settings/user-settings
-    
+    echo "Done."
     
     # Push all to git repo
+    # [Should probably warn that 'git add .' is run]
     git add .
     git commit -m "$2"
     git push
+
 elif [ "$1" == "-d" ]; then
     echo "Downloading dotfile settings..."
     git pull
+    
+    # RStudio
+    echo "RStudio config..."
+    mkdir -p ~/.rstudio-desktop/monitored/user-settings/
+    cp .rstudio-desktop/monitored/user-settings/user-settings ~/.rstudio-desktop/monitored/user-settings/user-settings
+    echo "Done."
+
 else
     echo "You must pass either '-u' or '-d' flags. Aborting." >&2
     exit 1
