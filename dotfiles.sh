364ffb865fee624e32e187eda9614b6490242fd7
Ryan Price <ryapric@gmail.com>
2018-10-29 17:29:37 -0500

    Move some of the config from the Ubuntu setup to this repo.

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$1" == "-u" ]; then
     if [ "$2" == "" ]; then
-        echo "If pushing dotfile changes, you must provide a git commit message as the second argument. Aborting." >&2
+        echo "ERROR: If pushing dotfile changes, you must provide a git commit message as the second argument. Aborting." >&2
         exit 1
     fi
 
@@ -32,6 +32,26 @@ elif [ "$1" == "-d" ]; then
     echo "Downloading dotfile settings..."
     git pull
 
+    # General/bash
+    if [ ! -e ~/.gitconfig ] || ! grep --quiet '[user]' ~/.gitconfig; then
+        echo -e "[user]
+        \tname = Ryan Price
+        \temail = ryapric@gmail.com" >> ~/.gitconfig
+    fi
+
+    echo '
+git_pull () {
+    for i in $(ls); do
+        if [ -e ${i}/.git ]; then
+            echo $i
+            git -C $i pull
+        fi
+    done
+}' >> ~/.bashrc
+    
+    echo '
+PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[01;33m\]$(__git_ps1)\[\033[00m\]\$ '"'"'' >> ~/.bashrc
+
     # RStudio
     echo "RStudio config..."
     mkdir -p ~/.rstudio-desktop/monitored/user-settings/
@@ -45,7 +65,7 @@ elif [ "$1" == "-d" ]; then
     echo "Done."
 
 else
-    echo "You must pass either '-u' or '-d' flags. Aborting." >&2
+    echo "ERROR: You must pass either '-u' or '-d' flags. Aborting." >&2
     exit 1
 fi
 
