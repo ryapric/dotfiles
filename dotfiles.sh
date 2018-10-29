48e93f571d4fca169b855bb0b7e153e307dad7bf
Ryan Price <ryapric@gmail.com>
2018-10-29 15:20:28 -0500

    Add spyder3 configs.

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -e
 
 
 if [ "$1" == "-u" ]; then
@@ -7,15 +7,21 @@ if [ "$1" == "-u" ]; then
         echo "If pushing dotfile changes, you must provide a git commit message as the second argument. Aborting." >&2
         exit 1
     fi
-    
+
     echo "Uploading dotfile settings..."
-    
+
     # RStudio
     echo "RStudio config..."
     mkdir -p .rstudio-desktop/monitored/user-settings/
     cp ~/.rstudio-desktop/monitored/user-settings/user-settings .rstudio-desktop/monitored/user-settings/user-settings
     echo "Done."
-    
+
+    # Spyder3
+    echo "Spyder3 config..."
+    mkdir -p .config/spyder-py3/
+    cp ~/.config/spyder-py3/spyder.ini .config/spyder-py3/spyder.ini
+    echo "Done."
+
     # Push all to git repo
     # [Should probably warn that 'git add .' is run]
     git add .
@@ -25,11 +31,17 @@ if [ "$1" == "-u" ]; then
 elif [ "$1" == "-d" ]; then
     echo "Downloading dotfile settings..."
     git pull
-    
+
     # RStudio
     echo "RStudio config..."
     mkdir -p ~/.rstudio-desktop/monitored/user-settings/
     cp .rstudio-desktop/monitored/user-settings/user-settings ~/.rstudio-desktop/monitored/user-settings/user-settings
+    echo "Done."
+
+    # Spyder 3
+    echo "Spyder 3 config..."
+    mkdir -p ~/.config/spyder-py3/
+    cp .config/spyder-py3/spyder.ini ~/.config/spyder-py3/spyder.ini
     echo "Done."
 
 else
